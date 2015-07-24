b866e9e25c2b0aa8fe757844444f1bd3159f0db4
vassildinev <vassildinev@gmail.com>
2015-07-24 22:53:31 +0300

    Added DefensiveProgrammingAndExceptionsHomework

---
+++ 08.DefensiveProgrammingAndExceptions/Exceptions-Homework/SimpleMathExam.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class SimpleMathExam : Exam
+{
+    private int problemsSolved;
+
+    public SimpleMathExam(int problemsSolved)
+    {
+        this.ProblemsSolved = problemsSolved;
+    }
+
+    public int ProblemsSolved 
+    { 
+        get
+        {
+            return this.problemsSolved;
+        }
+
+        private set
+        {
+            if (value < 0 || value > 2)
+            {
+                throw new ArgumentOutOfRangeException("Invalid problems solved count");
+            }
+
+            this.problemsSolved = value;
+        }
+    }
+
+    public override ExamResult Check()
+    {
+        if (this.ProblemsSolved == 0)
+        {
+            return new ExamResult(2, 2, 6, "Bad result: nothing done.");
+        }
+        else if (this.ProblemsSolved == 1)
+        {
+            return new ExamResult(4, 2, 6, "Average result: half done.");
+        }
+        else
+        {
+            return new ExamResult(6, 2, 6, "Excellent result: fully done.");
+        }
+    }
+}
