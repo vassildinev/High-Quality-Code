e662e9068751aef91d6b7e414103f6abde93bbec
vassildinev <vassildinev@gmail.com>
2015-08-20 15:19:08 +0300

    Added Development Tools homework

---
+++ 13.DevelopmentToolsHomework/01.Tools/Log4NetTask/Log4Net/Log4Net/Log4NetTest.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace Log4Net
+{
+    public class Log4NetTest
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetTest));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            var someSpecialValue = 42;
+            try {
+                for (var i = 0; i < 50; i++)
+                {
+                    if (i == someSpecialValue)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                    Log.Info(i + " jumping sheep named Pesho");
+                }
+            }
+
+            catch
+            {
+                Log.Error("Oh, no. Something went horribly wrong...");
+            }
+        }
+    }
+}
